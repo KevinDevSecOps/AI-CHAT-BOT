1c590cfdc3f3bd2b9f8ed31947a902a15d1e04de
CK <kevinpamiestrabajo@gmail.com>
2025-06-12 11:37:47 +0200

    Create Packages.js

---
+++ Packages.js
@@ -0,0 +1,21 @@
+{
+  "name": "simple-ai-chatbot",
+  "version": "1.0.0",
+  "description": "Un chatbot básico de IA en JavaScript para la terminal.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "keywords": [
+    "chatbot",
+    "ia",
+    "javascript",
+    "consola",
+    "nlp"
+  ],
+  "author": "KevinDevSecOps",
+  "license": "MIT",
+  "dependencies": {
+    "readline-sync": "^1.4.10"
+  }
+}
